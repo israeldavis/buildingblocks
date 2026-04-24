f9a3dbbe7d2cffc6c1d818954752e2cb4438a034
israeldavis <dvences90@gmail.com>
2026-04-24 10:29:00 -0600

    First commit

---
+++ src/test/java/com/simplify/restservices/BuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplify.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
